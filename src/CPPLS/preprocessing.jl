fe28ed938d8ec96e4d857b5c9d4e0900e7885650
Oliver Niehuis <oliver.niehuis@biologie.uni-freiburg.de>
2026-03-21 19:17:39 +0100

    Testing

--- src/CPPLS/preprocessing.jl
+++ src/CPPLS/preprocessing.jl
@@ -1,47 +1,83 @@
 """
-    colmean(M::AbstractMatrix{<:Real})
+    colmean(M::AbstractMatrix{<:Real}, ::Nothing)
     colmean(M::AbstractMatrix{<:Real}, obs_weights::AbstractVector{<:Real})
 
 Compute the (optionally weighted) column means of M.
 """
-colmean(M::AbstractMatrix{<:Real}) = mean(M, dims=1)
-colmean(M::AbstractMatrix{<:Real}, obs_weights::AbstractVector{<:Real}) = (obs_weights' * M) / sum(obs_weights)
+colmean(M::AbstractMatrix{<:Real}, ::Nothing) = mean(M, dims=1)
+colmean(M::AbstractMatrix{<:Real}, obs_weights::AbstractVector{<:Real}) = 
+    (obs_weights' * M) / sum(obs_weights)
 
 """
-    colstd(M::AbstractMatrix{<:Real})
+    colstd(M::AbstractMatrix{<:Real}, ::Nothing)
+    colstd(M::AbstractMatrix{<:Real}, ::Nothing, μ::AbstractVector{<:Real})
     colstd(M::AbstractMatrix{<:Real}, obs_weights::AbstractVector{<:Real})
+    colstd(M::AbstractMatrix{<:Real}, obs_weights::AbstractVector{<:Real}, μ::AbstractVector{<:Real})
 
-Compute the (optionally weighted) column standard deviations of M.
+Compute the (optionally weighted) column standard deviations of `M`.
 """
-colstd(M::AbstractMatrix{<:Real}) = std(M, dims=1)
-colstd(M::AbstractMatrix{<:Real}, obs_weights::AbstractVector{<:Real}) = begin
+function colstd(M::AbstractMatrix{<:Real}, ::Nothing)
+    μ = colmean(M, nothing)
+    sqrt.(sum((M .- μ).^2, dims=1) / (size(M, 1) - 1))
+end
+function colstd(M::AbstractMatrix{<:Real}, obs_weights::AbstractVector{<:Real})
     μ = colmean(M, obs_weights)
+    sqrt.(sum(obs_weights .* (M .- μ).^2, dims=1) / sum(obs_weights))
+end
+function colstd(
+    M::AbstractMatrix{<:Real}, 
+    ::Nothing, 
+    μ::AbstractVector{<:Real}
+)
+    sqrt.(sum((M .- μ).^2, dims=1) / (size(M, 1) - 1))
+end
+function colstd(
+    M::AbstractMatrix{<:Real}, 
+    obs_weights::AbstractVector{<:Real}, 
+    μ::AbstractVector{<:Real}
+)
     sqrt.(sum(obs_weights .* (M .- μ).^2, dims=1) / sum(obs_weights))
 end
 
 """
-    center_and_scale(M::AbstractMatrix{<:Real}; center::Bool=true, scale::Bool=true, obs_weights=nothing)
+    center_and_scale(
+        M::AbstractMatrix{<:Real},
+        center::Bool, 
+        scale::Bool, 
+        obs_weights::Union{AbstractVector{<:Real}, Nothing}
+)
 
 Center and/or scale the columns of M. Returns (M_trans, means, stds).
 If obs_weights is provided, use weighted mean and std.
 """
-function center_and_scale(M::AbstractMatrix{<:Real}; center::Bool=true, scale::Bool=true, obs_weights=nothing)
+function center_and_scale(
+    M::AbstractMatrix{<:Real}, 
+    center::Bool, 
+    scale::Bool, 
+    obs_weights::Union{AbstractVector{<:Real}, Nothing}
+)
     if center
-        μ = isnothing(obs_weights) ? colmean(M) : colmean(M, obs_weights)
+        μ = colmean(M, obs_weights)
         M_centered = M .- μ
     else
         μ = zeros(1, size(M,2))
         M_centered = M
     end
-    if scale
-        σ = isnothing(obs_weights) ? colstd(M_centered) : colstd(M_centered, obs_weights)
+
+    if scale && center
+        σ = colstd(M_centered, obs_weights, μ)
         M_scaled = M_centered ./ σ
+    elseif scale && !center
+        σ = colstd(M, obs_weights)
+        M_scaled = M ./ σ
     else
         σ = ones(1, size(M,2))
         M_scaled = M_centered
     end
-    return M_scaled, μ, σ
+
+    M_scaled, μ, σ
 end
+
 """
     center_mean(M::AbstractMatrix{<:Real}, obs_weights::AbstractVector{<:Real})
     center_mean(M::AbstractMatrix{<:Real}, ::Nothing)
@@ -131,26 +167,20 @@ function cppls_prepare_data(
     obs_weights::Union{AbstractVector{<:Real}, Nothing},
     center::Bool
 )
-
     X = convert_to_float64(X)
     Y_prim = convert_to_float64(Y_prim)
-
     if !isnothing(Y_aux)
         Y_aux = convert_auxiliary_to_float64(Y_aux)
     end
-
     Y = isnothing(Y_aux) ? Y_prim : hcat(Y_prim, Y_aux)
-
     n_samples_X, n_features_X = size(X)
     n_samples_Y, n_targets_Y = size(Y_prim)
     n_samples_X ≠ n_samples_Y && throw(
         DimensionMismatch("Number of rows in X and Y_prim must be equal"))
-
     if !isnothing(obs_weights) && length(obs_weights) ≠ n_samples_X
         throw(DimensionMismatch(
             "Length of observation_weights must match the number of rows in X and Y_prim"))
     end
-
     if center
         X, X_bar = center_mean(X, obs_weights)
         Y_bar = mean(Y_prim, dims = 1)
@@ -158,14 +188,12 @@ function cppls_prepare_data(
         X_bar = zeros(Float64, (1, n_features_X))
         Y_bar = zeros(Float64, (1, n_targets_Y))
     end
-
     X_def = copy(X)
     W_comp = Matrix{Float64}(undef, n_features_X, ncomponents)
     P = Matrix{Float64}(undef, n_features_X, ncomponents)
     C = Matrix{Float64}(undef, n_targets_Y, ncomponents)
     zero_mask = Matrix{Bool}(undef, (ncomponents, n_features_X))
     B = Array{Float64}(undef, (n_features_X, n_targets_Y, ncomponents))
-
     (X, Y_prim, Y, obs_weights, X_bar, Y_bar, X_def, W_comp, P, C, zero_mask, B,
         n_samples_X, n_targets_Y)
 end
